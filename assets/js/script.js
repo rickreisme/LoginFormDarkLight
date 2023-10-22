bed058896a670900295b5880d73a268cdfb09aaf
Rick <ricleyreis@gmail.com>
2023-10-21 21:07:25 -0300

    ajustes de cor e fonte, javascript social media

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 const modo = document.getElementById('mode_icon');
+const git = document.getElementById('git-icon');
+const goog = document.getElementById('google-icon');
+const face = document.getElementById('face-icon');
 
 modo.addEventListener('click', () => {
     const form = document.getElementById('login_form');
@@ -6,13 +9,19 @@ modo.addEventListener('click', () => {
     if(modo.classList.contains('fa-moon')){
         modo.classList.remove('fa-moon');
         modo.classList.add('fa-sun');
-
         form.classList.add('dark');
+
+        git.style.color = 'white';
+        goog.style.color = '#0FD90F';
+        face.style.color = '#1056e0';
     }
     else if(modo.classList.contains('fa-sun')){
         modo.classList.remove('fa-sun');
         modo.classList.add('fa-moon')
-
         form.classList.remove('dark');
+
+        git.style.color = 'black';
+        goog.style.color = '#076907';
+        face.style.color = '#355390';
     }
 });
